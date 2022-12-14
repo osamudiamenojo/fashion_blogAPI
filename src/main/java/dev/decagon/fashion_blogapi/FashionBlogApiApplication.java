810135629f8a77f2f8ed1b0af86dea9e8870f0d3
Ojo Victor <osamudiamenojo@users.noreply.github.com>
2022-12-14 02:27:47 +0100

    first

--- src/main/java/dev/decagon/fashion_blogapi/FashionBlogApiApplication.java
+++ src/main/java/dev/decagon/fashion_blogapi/FashionBlogApiApplication.java
@@ -8,6 +8,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
@@ -15,15 +16,9 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class FashionBlogApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(FashionBlogApiApplication.class, args);
     }
 
-    @Bean
-    public Docket swaggerConfiguration() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("dev.decagon.fashion_blogapi"))
-                .paths(PathSelectors.ant("/api/v1/*"))
-                .build();
-    }
 
 }
